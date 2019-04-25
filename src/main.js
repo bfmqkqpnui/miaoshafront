925c58603fd5eb2b7005d91af642ad7666106b6a
bfmqkqpnui <47877962@qq.com>
2019-04-26 01:23:59 +0800

    优化代码

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import FastClick from 'fastclick'
+// import FastClick from 'fastclick'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import router from './router'
@@ -21,6 +21,7 @@ Vue.use(VueRouter)
 Vue.use(ToastPlugin)
 // 使用微信 jssdk
 Vue.use(WechatPlugin)
+const FastClick = require('fastclick')
 FastClick.attach(document.body)
 Vue.config.productionTip = false
 
